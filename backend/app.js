8506cca01b35c1abec01ebf5d00240903addf306
darya55k <darya55k2001@yandex.ru>
2021-08-25 16:45:57 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,13 @@ const app = express();
   'https://mesto.darya55k.nomoredomains.monster',
 ];
 */
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://mesto.darya55k.nomoredomains.monster',
+    'http://mesto.darya55k.nomoredomains.monster',
+    'http://localhost:3000',
+  ],
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
