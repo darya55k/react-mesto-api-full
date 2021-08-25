e86f5fa8311f3a0369497a824af89dbc970d4ec8
darya55k <darya55k2001@yandex.ru>
2021-08-25 22:29:16 +0300

    kkk

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -15,25 +15,16 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-/* const allowedCors = [
+const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
   'http://localhost:3000',
   'http://mesto.darya55k.nomoredomains.monster',
   'https://mesto.darya55k.nomoredomains.monster',
 ];
-*/
-app.use(cors({
-  origin: [
-    'https://mesto.darya55k.nomoredomains.monster',
-    'http://mesto.darya55k.nomoredomains.monster',
-    'http://localhost:3000',
-  ],
-}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(requestLogger);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -42,7 +33,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['accept-languageaccess-control-request-headers'];
@@ -60,7 +51,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
   return next();
 });
-*/
+
+app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
