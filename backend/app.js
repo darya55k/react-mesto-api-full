a3a6a7cdc367b8b52b7d05a7b8d6eaba87f514e8
darya55k <darya55k2001@yandex.ru>
2021-08-25 23:42:04 +0300

    gg

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -15,14 +15,14 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const allowedCors = [
+/* const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
   'http://localhost:3000',
   'http://mesto.darya55k.nomoredomains.monster',
   'https://mesto.darya55k.nomoredomains.monster',
-];
-
+]; */
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -33,7 +33,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['accept-languageaccess-control-request-headers'];
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
   }
 
   return next();
-});
+}); */
 
 app.use(requestLogger);
 
