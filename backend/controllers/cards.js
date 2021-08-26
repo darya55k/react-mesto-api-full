371ddfc49014fa2fa910cc5ccf63f8e6a20105a1
darya55k <darya55k2001@yandex.ru>
2021-08-27 00:34:33 +0300

    m

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -16,11 +16,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((createdCard) => {
-      const card = createdCard.toObject();
-      card.owner = req.user;
-      return res.status(201).send({ data: card });
-    })
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные'));
