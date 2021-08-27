3d939d113aa0da5357728b314b2780cf0743baaa
darya55k <darya55k2001@yandex.ru>
2021-08-27 15:48:49 +0300

    f

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -16,8 +16,10 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => {
-      res.status(200).send(card);
+    .then((createdCard) => {
+      const card = createdCard.toObject();
+      card.owner = req.user;
+      return res.status(201).send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
